c457bb159a538913d18513daf89dc7166fbde5b5
ronnelsidevians <m7nt47vg98@privaterelay.appleid.com>
2026-05-14 10:54:13 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'dsi-v1-cache';
+// Тут перерахуйте всі ваші файли, які треба зберегти
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './data1.json',
+  './data2.json',
+  './data3.json',
+  './data4.json',
+  './data5.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
