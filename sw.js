c49cf2745a56ecefc6a9e488a89f262f3dabb480
ronnelsidevians <m7nt47vg98@privaterelay.appleid.com>
2026-05-15 11:45:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dsi-v3-cache'; // Змінюйте цифру щоразу, коли щось міняєте в коді!
+const CACHE_NAME = 'dsi-v22-cache'; // Змінюйте цифру щоразу, коли щось міняєте в коді!
 
 const urlsToCache = [
   './',
